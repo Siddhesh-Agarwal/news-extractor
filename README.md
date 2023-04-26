226348c8a30568a1942cf1e52d61cb7ef541712a
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-04-26 12:33:34 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# News extractor
+
+A simple news extractor that extracts the news from the given url and displays it in a simple UI. It can bypass the paywalls and ad blocks to extract the news.
+
+____
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the required packages.
+
+```bash
+pip install -r requirements.txt
+```
+
+____
+
+## Usage
+
+```bash
+python app.py
+```
+
+____
+
+## Tools 
+
+Made using:
+
+- [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
+- [requests](https://requests.readthedocs.io/en/master/)
+- [flask](https://flask.palletsprojects.com/en/latest/)
---
+++ app.py
@@ -0,0 +1,56 @@
+from typing import List, Tuple
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
+
+
+def get_news(url: str) -> Tuple[List[str], str]:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/91.0.4472.114 Safari/537.36"
+    }
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        # Get image source
+        image = soup.find("source")
+        source = image["srcset"] if image else ""
+        if not any(
+            [source.endswith("jpg"), source.endswith("png"), source.endswith("jpeg")]
+        ):
+            image = soup.find("img")
+            source = image["src"] if image else ""
+        print(f"{source=}")
+
+        # fetch news and clean it
+        news = soup.find_all("p")
+        clean_news = []
+        for item in news:
+            item = item.text.strip()
+            if len(item) > 50:
+                clean_news.append(item)
+
+        return clean_news, source
+    return [], ""
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", news=[])
+
+
+@app.route("/", methods=["POST"])
+def news():
+    url = request.form["url"]
+    news, src = get_news(url)
+    # pprint(src)
+    return render_template("index.html", news=news, url=url, src=src)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
